b93c02814d45ee799a03a747eb92c50110d90996
giahuydo99 <giahuydo99@gmail.com>
2020-01-09 11:03:33 +1030

    Added travelling to each game. Now working on opening player markets

--- Bet365Collector.py
+++ Bet365Collector.py
@@ -4,26 +4,42 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import time
 
-
-
+#Class Retrieves page_source from Bet365 websites
 class Bet365Collector:
-
+	#Input: URL of sport
 	def __init__(self, url):
 		self.url = url
 
+	#Returns a list of page_sources. Corresponding to today's games.
 	def get_page_sources(self, games):
 		page_sources = []
 		driver = Chrome()
 		driver.get(self.url)
+		time.sleep(3)
 
-		print(driver.page_source)
+		#Retrieves all game links
+		games = driver.find_elements_by_class_name("sl-CouponParticipantGameLineTwoWay_NameText")
 
-		game = driver.find_element_by_class_name("sl-CouponParticipantGameLineTwoWay_NameText")
-		game[0].click()
+		#Travels to each game
+		i = 0
+		while i < len(games):
+			games = driver.find_elements_by_class_name("sl-CouponParticipantGameLineTwoWay_NameText")
+			game = games[i];
+			i += 2
+			game.click()
+			time.sleep(2)
+
+			#Tries to find player markets
+			
+
+			driver.back()
+			time.sleep(2)
+
+		return games
 
 
 collector = Bet365Collector("https://www.bet365.com.au/#/AC/B18/C20604387/D48/E1453/F10/")
-games = collector.get_page_sources(collector.url)
+game = collector.get_page_sources(collector.url)
 
 
 #sl-CouponParticipantGameLineTwoWay_Name
