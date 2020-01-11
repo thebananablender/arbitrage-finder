71c47e0cad4050501be196de1903ba0de7641ddb
giahuydo99 <giahuydo99@gmail.com>
2020-01-11 16:22:57 +1030

    Collector alpha version finish. Now working on Scraper.

--- Bet365Collector.py
+++ Bet365Collector.py
@@ -29,17 +29,45 @@ class Bet365Collector:
 			game.click()
 			time.sleep(2)
 
-			#Tries to find player markets
-			
+			page_sources.append(self.get_game_page_source(driver))
 
+			#Returns to previous page
+			driver.back()
+			time.sleep(2)
 			driver.back()
 			time.sleep(2)
 
-		return games
+		return page_sources
+
+	def get_game_page_source(self, driver):
+		try:
+			#Tries to find the player-market tab
+			driver.find_element_by_xpath("//div[contains(text(), 'Player Markets')]").click()
+			time.sleep(2)
+
+			#Tries to ppen assists, rebounds and all show all dropdowns
+			try:
+				driver.find_element_by_xpath("//div[contains(text(), 'Player Assists')]").click()
+				time.sleep(3)
+
+				driver.find_element_by_xpath("//div[contains(text(), 'Player Rebounds')]").click()
+				time.sleep(3)
+
+				showMoreDropdowns = driver.find_elements_by_class_name("msl-ShowMore")
+				for dropdown in showMoreDropdowns:
+					dropdown.click()
+					time.sleep(3)
+
+			except NoSuchElementException:
+				print("Either assist, rebound was not found. Exporting data anyway")
+
+			#Returns the page source
+			return driver.page_source
+
+		except NoSuchElementException:
+			print("Player Markets Not Found")
+			return "Player Markets Not Found"
 
 
 collector = Bet365Collector("https://www.bet365.com.au/#/AC/B18/C20604387/D48/E1453/F10/")
-game = collector.get_page_sources(collector.url)
-
-
-#sl-CouponParticipantGameLineTwoWay_Name
+games = collector.get_page_sources(collector.url)
