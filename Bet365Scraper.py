c1b968e702b71d3f8025bf9d97cf3e74c2938ecc
giahuydo99 <giahuydo99@gmail.com>
2020-01-11 16:29:25 +1030

    Getting scrape_data to send containers

--- Bet365Scraper.py
+++ Bet365Scraper.py
@@ -8,5 +8,17 @@ class Bet365Scraper:
 		self.sources = sources
 
 	def scrape_data(self, source):
+		
 		soup = BeautifulSoup(source, 'html.parser')
+		
+		titles = soup.find_all("div", {"class":"gl-MarketGroupButton_Text"})
+
+		for title in titles:
+			if title.get_text() == "Player Points Markets":
+				points_Title = title
+			if title.get_text() == "Player Assists Markets":
+				assists_Title = title
+			if title.get_text() == "Player Rebounds Markets":
+				rebounds_Title = title
+
 		
