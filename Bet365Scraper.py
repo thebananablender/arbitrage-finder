f6db0fa2d63476219ff863bb32f68b02839713dd
giahuydo99 <giahuydo99@gmail.com>
2020-01-13 17:16:59 +1030

    Creating the barebones script. Need player markets to be open to actually make it work

--- Bet365Scraper.py
+++ Bet365Scraper.py
@@ -14,11 +14,20 @@ class Bet365Scraper:
 		titles = soup.find_all("div", {"class":"gl-MarketGroupButton_Text"})
 
 		for title in titles:
-			if title.get_text() == "Player Points Markets":
+			if title.get_text() == "Player Points":
 				points_Title = title
-			if title.get_text() == "Player Assists Markets":
+			if title.get_text() == "Player Assists":
 				assists_Title = title
-			if title.get_text() == "Player Rebounds Markets":
+			if title.get_text() == "Player Rebounds":
 				rebounds_Title = title
 
-		
+		points_Container = points_Title.find_next()
+		assists_Container = points_Title.find_next()
+		rebounds_Container = points_Title.find_next()
+
+		return points_Container, assists_Container, rebounds_Container
+
+
+	def write_to_csv(self, points_Container, assists_Container, rebounds_Container):
+		with open('sportsbet_Odds.csv', 'w') as csv_file:
+			csv_writer = writer(csv_file)
